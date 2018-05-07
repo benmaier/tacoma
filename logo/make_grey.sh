d75d12667497a1f0b01cd93a4c312c3d3d086a2b
Benjamin Maier <benjaminfrankmaier@gmail.com>
2018-05-07 23:53:42 +0200

    added small logo and made strokes thicc

--- logo/make_grey.sh
+++ logo/make_grey.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 convert new_logo.png -set colorspace Gray -separate -average new_logo_grey.png
+convert new_logo_small.png -set colorspace Gray -separate -average new_logo_small_grey.png
