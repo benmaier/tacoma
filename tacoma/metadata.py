7c0c619f259160c010436df1ef699c2a80f02f21
Benjamin Maier <benjaminfrankmaier@gmail.com>
2018-12-01 23:27:13 +0100

    v++

--- tacoma/metadata.py
+++ tacoma/metadata.py
@@ -3,7 +3,7 @@
 Contains a bunch of information about this package
 """
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 __author__ = "Benjamin F. Maier"
 __copyright__ = "Copyright 2018, Benjamin F. Maier"
